c037dafd707e3ecf1c497ace9bc7ee066ffecfdc
Nick Smedira <nsmedira@me.com>
2020-07-17 16:39:52 -0400

    added official solution (using set())

---
+++ problem_1_officialSolution_set.js
@@ -0,0 +1,13 @@
+function two_sum ( list, k ) {
+    let seen = new Set()
+    for ( let i = 0 ; i < list.length ; i++ ) {
+        let number = k - list[i]
+        if (seen.has(number)) {
+            return true
+        }
+        seen.add(list[i])
+    }
+    return false
+}
+
+console.log(two_sum([9,1,9,1], 18))
